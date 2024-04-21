82d7c617ce3f1f2a9c16da67b5be4f78b61814e9
Mykola Mykhaliuk <mykola.mykhaliuk@gmail.com>
2024-04-21 18:07:01 +0200

    Add bootstrap login

--- WebAPP/src/app/app.module.ts
+++ WebAPP/src/app/app.module.ts
@@ -3,21 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule, HttpClient, HTTP_INTERCEPTORS, withInterceptors, provideHttpClient } from '@angular/common/http';
+import {
+  HttpClientModule,
+  HttpClient,
+  HTTP_INTERCEPTORS,
+  withInterceptors,
+  provideHttpClient,
+} from '@angular/common/http';
 import { authInterceptor } from './services/auth.interceptor';
-import { LoginComponent } from './components/login/login.component';
-import { HomeComponent } from './components/home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgbModule],
   providers: [provideHttpClient(withInterceptors([authInterceptor]))],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
